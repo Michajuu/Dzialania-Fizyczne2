b327096d093fd4f641ef053d565ce91b26060fe9
Michajuu <73675435+Michajuu@users.noreply.github.com>
2021-04-21 09:04:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function Oblicz()
+{
+    let m = document.getElementById("m").value;
+    let Ep = document.getElementById("Ep").value;
+let a = document.getElementById("Φ");
+a.innerHTML = Ep / m;
+}
